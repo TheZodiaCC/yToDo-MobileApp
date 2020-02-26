a7d8630d8347342405807a962b1a499583c183d7
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2020-02-26 10:30:11 +0100

    Add Add Activity

--- App/app/src/main/java/com/example/ytodomobile/MainActivity.java
+++ App/app/src/main/java/com/example/ytodomobile/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.ytodomobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button add_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add_button = (Button) findViewById(R.id.add);
+        //add_button.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        openActivityAdd();
+        //    }
+        //});
+    }
+    public void openActivityAdd()
+    {
+        Intent intent = new Intent(this, AddActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                openActivityAdd();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
